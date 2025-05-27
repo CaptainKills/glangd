b78166042e5cd6448e480eec4d5c080ffe3b6174
Danick Triantis <danick@triantis.nl>
2025-05-28 00:05:40 +0200

    Commands are now stored and written to array.

--- glangd.go
+++ glangd.go
@@ -21,7 +21,7 @@ const (
 type CompileCommand struct {
 	Directory  string
 	Command    string
-	SourceFile string
+	File string
 }
 
 func (c CompileCommand) ToString() string {
@@ -38,7 +38,7 @@ func (c CompileCommand) ToString() string {
 	builder.WriteString("\",\n")
 
 	builder.WriteString("\t\t\"file\": \"")
-	builder.WriteString(c.SourceFile)
+	builder.WriteString(c.File)
 	builder.WriteString("\"\n")
 
 	builder.WriteString("\t}")
@@ -61,38 +61,39 @@ func main() {
 		log.Fatalf("Could not compile directory regex! %q\n", err)
 	}
 
-	sourceFileRegex, err := regexp.Compile(sourceFileRegex)
-	if err != nil {
-		log.Fatalf("Could not compile directory regex! %q\n", err)
-	}
+	// sourceFileRegex, err := regexp.Compile(sourceFileRegex)
+	// if err != nil {
+	// 	log.Fatalf("Could not compile directory regex! %q\n", err)
+	// }
 
 	fileRegex, err := regexp.Compile(fileNameRegex)
 	if err != nil {
 		log.Fatalf("Could not compile directory regex! %q\n", err)
 	}
 
+	var compileCommands []CompileCommand
+	var command CompileCommand
+
 	for _, line := range lines {
 		if strings.Contains(line, "directory") {
 			directory := strings.ReplaceAll(directoryRegex.FindString(line), "'", "")
 			fmt.Printf("(Directory) %s\n", directory)
+
+			command.Directory = directory
 		}
 
 		if strings.Contains(line, ".c") {
-			sourceFile := sourceFileRegex.FindString(line)
-			fmt.Printf("(Source File) %s\n", sourceFile)
+			// sourceFile := sourceFileRegex.FindString(line)
+			// fmt.Printf("(Source File) %s\n", sourceFile)
 			file := fileRegex.FindString(line)
 			fmt.Printf("(File) %s\n", file)
+
+			command.Command = line
+			command.File = file
+			compileCommands = append(compileCommands, command)
 		}
 
 	}
-
-	var compileCommands []CompileCommand
-	command := CompileCommand{Directory: "/home/danick/", Command: "gcc -o main main.c", SourceFile: "main.c"}
-	compileCommands = append(compileCommands, command)
-	command = CompileCommand{Directory: "/home/senno/", Command: "gcc -o main /home/senno/src/main.c", SourceFile: "src/tree.c"}
-	compileCommands = append(compileCommands, command)
-	command = CompileCommand{Directory: "/home/tonnie/", Command: "gcc -o main main.c", SourceFile: "file.c"}
-	compileCommands = append(compileCommands, command)
 
 	// Write to compile_commands.json
 	writeFile(outputFileName, compileCommands)
