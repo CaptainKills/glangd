cb05829f61dbf12362219a347a5293260d54cf32
Danick Triantis <danick@triantis.nl>
2025-08-26 22:26:59 +0200

    feat: split regex to seperate file.

---
+++ regex.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"regexp"
+)
+
+const (
+	// Regex
+	compilerRegexExpr = "cc|gcc|clang|riscv32-unknown-elf-gcc"
+	pathRegexExpr     = "([^\\s]+[.]c)"
+	fileRegexExpr     = "[a-zA-Z0-9_-]+[.]c"
+)
+
+var (
+	CompilerRegex *regexp.Regexp
+	PathRegex     *regexp.Regexp
+	FileRegex     *regexp.Regexp
+)
+
+func InitRegex() {
+	var err error
+
+	CompilerRegex, err = regexp.Compile(compilerRegexExpr)
+	if err != nil {
+		log.Fatalf("Could not compile 'compiler' regex! %q\n", err)
+	}
+
+	PathRegex, err = regexp.Compile(pathRegexExpr)
+	if err != nil {
+		log.Fatalf("Could not compile 'path' regex! %q\n", err)
+	}
+
+	FileRegex, err = regexp.Compile(fileRegexExpr)
+	if err != nil {
+		log.Fatalf("Could not compile 'file' regex! %q\n", err)
+	}
+}
---
+++ regex_test.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestCompilerRegex(t *testing.T) {
+	t.Run("Compiler: cc", func(t *testing.T) {
+		command := "cc -Wall -Wextra -pedantic -O0 -g3 -std=c99 -I. -I../vep_0/libbsp/include  -L. -L../vep_0/libchannel/lib  channel_init.c  -lchannel-arm -lm -o channel_init"
+
+		regex, err := regexp.Compile(compilerRegexExpr)
+		if err != nil {
+			t.Fatalf("Could not compile 'compiler' regex! %q\n", err)
+		}
+
+		got := regex.FindString(command)
+		want := "cc"
+
+		if got != want {
+			t.Fatalf("got %q but wanted %q\n", got, want)
+		}
+	})
+
+	t.Run("Compiler: gcc", func(t *testing.T) {
+		command := "gcc -c -o build/main.o src/main.c -Wall -Wextra -Wformat -O3 -I inc/"
+
+		regex, err := regexp.Compile(compilerRegexExpr)
+		if err != nil {
+			t.Fatalf("Could not compile 'compiler' regex! %q\n", err)
+		}
+
+		got := regex.FindString(command)
+		want := "gcc"
+
+		if got != want {
+			t.Fatalf("got %q but wanted %q\n", got, want)
+		}
+	})
+
+	t.Run("Compiler: clang", func(t *testing.T) {
+		command := "clang /path/test.cc -DNDEBUG -fsyntax-only -resource-dir=/usr/lib/clang/lib/12/"
+
+		regex, err := regexp.Compile(compilerRegexExpr)
+		if err != nil {
+			t.Fatalf("Could not compile 'compiler' regex! %q\n", err)
+		}
+
+		got := regex.FindString(command)
+		want := "clang"
+
+		if got != want {
+			t.Fatalf("got %q but wanted %q\n", got, want)
+		}
+	})
+
+	t.Run("Compiler: riscv32-unknown-elf-gcc", func(t *testing.T) {
+		command := "riscv32-unknown-elf-gcc -Iinclude -c -o libsrc/greyscale.o  libsrc/greyscale.c"
+
+		regex, err := regexp.Compile(compilerRegexExpr)
+		if err != nil {
+			t.Fatalf("Could not compile 'compiler' regex! %q\n", err)
+		}
+
+		got := regex.FindString(command)
+		want := "riscv32-unknown-elf-gcc"
+
+		if got != want {
+			t.Fatalf("got %q but wanted %q\n", got, want)
+		}
+	})
+}
+
+func TestPathRegex(t *testing.T) {
+	command := "gcc -c -o build/main.o src/main.c -Wall -Wextra -Wformat -O3 -I inc/"
+
+	regex, err := regexp.Compile(pathRegexExpr)
+	if err != nil {
+		t.Fatalf("Could not compile 'path' regex! %q\n", err)
+	}
+
+	got := regex.FindString(command)
+	want := "src/main.c"
+
+	if got != want {
+		t.Fatalf("got %q but wanted %q\n", got, want)
+	}
+}
+
+func TestFileRegex(t *testing.T) {
+	t.Run("File: main.c", func(t *testing.T) {
+		command := "gcc -c -o build/main.o src/main.c -Wall -Wextra -Wformat -O3 -I inc/"
+
+		regex, err := regexp.Compile(fileRegexExpr)
+		if err != nil {
+			t.Fatalf("Could not compile 'file' regex! %q\n", err)
+		}
+
+		got := regex.FindString(command)
+		want := "main.c"
+
+		if got != want {
+			t.Fatalf("got %q but wanted %q\n", got, want)
+		}
+	})
+
+	t.Run("File: channel_init.c", func(t *testing.T) {
+		command := "cc -Wall -Wextra -pedantic -O0 -g3 -std=c99 channel_init.c  -lchannel-arm -lm -o channel_init"
+
+		regex, err := regexp.Compile(fileRegexExpr)
+		if err != nil {
+			t.Fatalf("Could not compile 'file' regex! %q\n", err)
+		}
+
+		got := regex.FindString(command)
+		want := "channel_init.c"
+
+		if got != want {
+			t.Fatalf("got %q but wanted %q\n", got, want)
+		}
+	})
+
+	t.Run("File: generate-json.c", func(t *testing.T) {
+		command := "gcc -Wall -Wextra -pedantic -O0 -g3 -std=c99 -o generate-json generate-json.c ../vep_0/libbsp/libsrc/platform.c -lchannel-arm -lm"
+
+		regex, err := regexp.Compile(fileRegexExpr)
+		if err != nil {
+			t.Fatalf("Could not compile 'file' regex! %q\n", err)
+		}
+
+		got := regex.FindString(command)
+		want := "generate-json.c"
+
+		if got != want {
+			t.Fatalf("got %q but wanted %q\n", got, want)
+		}
+	})
+}
